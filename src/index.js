6ade7c29b976a800c8c7229acb846cc0583b9f44
Andre Gonzales <andreg-16@hotmail.com>
2022-05-25 17:17:18 -0500

    Drop Tables

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import './models';
 
 async function main() {
     try {
-        await sequelize.sync({ alter: true });
+        await sequelize.drop();
+        await sequelize.sync();
         console.log('Database connected');
     } catch (error) {
         console.error('Unable to connect to database');
